215df9d5adb464b6c399f2610770cfb801e7e6c4
Dominic Saladin <dominic@dsaladin.ch>
2024-11-22 23:04:05 +0100

    Add docker support

---
+++ CloudflareDNSUpdater/CloudflareDNSUpdater/LoggerHostedService.cs
@@ -0,0 +1,120 @@
+﻿using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mime;
+using System.Text;
+using System.Text.Json.Nodes;
+using System.Threading.Tasks;
+
+namespace DSaladin.CloudflareDnsUpdater
+{
+    internal class LoggerHostedService : IHostedService, IDisposable
+    {
+        private readonly ILogger<LoggerHostedService> logger;
+        private Timer? _timer;
+
+        private TimeSpan period;
+        private string? bearerToken = "";
+        private List<string> zoneIdentifiers = [];
+
+        public LoggerHostedService(ILogger<LoggerHostedService> logger)
+        {
+            this.logger = logger;
+
+            bearerToken = Environment.GetEnvironmentVariable("Token");
+            string? zoneIdentifiersText = Environment.GetEnvironmentVariable("ZoneIdentifiers");
+
+            if (!string.IsNullOrEmpty(zoneIdentifiersText))
+                zoneIdentifiers = [.. zoneIdentifiersText.Split(",")];
+
+            if (bearerToken is null)
+            {
+                logger.LogError("The bearer token is empty. Please set the environment variable Token");
+                throw new NullReferenceException("The bearer token is empty");
+            }
+
+            if (zoneIdentifiers.Count == 0)
+            {
+                logger.LogError("No zone identifiers defined. Please set the environment variable ZoneIdentifiers");
+                throw new NullReferenceException("No zones found");
+            }
+
+            string? periodText = Environment.GetEnvironmentVariable("Period");
+            period = periodText is not null ? TimeSpan.Parse(periodText) : TimeSpan.FromMinutes(5);
+
+
+        }
+
+        public Task StartAsync(CancellationToken cancellationToken)
+        {
+            logger.LogInformation("Starting Cloudflare DNS Update service.");
+            _timer = new Timer(DoWork, null, TimeSpan.Zero, TimeSpan.FromMinutes(5));
+            return Task.CompletedTask;
+        }
+
+        private void DoWork(object? state)
+        {
+            HttpClient client = new();
+            client.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", bearerToken);
+
+            // Get current public/external ip
+            HttpResponseMessage ipResponseMessage = new HttpClient().Send(new(HttpMethod.Get, "http://icanhazip.com"));
+            string ip = ipResponseMessage.Content.ReadAsStringAsync().Result.Trim();
+
+            logger.LogInformation("Sending current ip to Cloudflare: {ip}", ip);
+
+            foreach (string zoneIdentifier in zoneIdentifiers)
+            {
+                HttpResponseMessage dnsRecordsResponseMessage = client.Send(new(HttpMethod.Get, $"https://api.cloudflare.com/client/v4/zones/{zoneIdentifier}/dns_records?type=A"));
+                JsonObject? dnsRecords = (JsonObject)JsonNode.Parse(dnsRecordsResponseMessage.Content.ReadAsStringAsync().Result)!;
+
+                if (dnsRecords == null || dnsRecords.Count == 0 || dnsRecords.First().Value == null || dnsRecords.First().Value!.GetType() != typeof(JsonArray))
+                    continue;
+
+                foreach (JsonObject? dnsRecord in dnsRecords.First().Value!.AsArray().Cast<JsonObject?>())
+                {
+                    if (dnsRecord is null)
+                        continue;
+
+                    string identifier = dnsRecord["id"]!.GetValue<string>();
+                    string dnsName = dnsRecord["name"]!.GetValue<string>();
+
+                    JsonObject bodyJson = new()
+                    {
+                        { "type", dnsRecord["type"]!.GetValue<string>() },
+                        { "name", dnsName },
+                        { "content", ip },
+                        { "ttl", dnsRecord["ttl"]!.GetValue<int>() },
+                        { "proxied", dnsRecord["proxied"]!.GetValue<bool>() }
+                    };
+
+                    logger.LogInformation("Updating IP for DNS record {dnsName}", dnsName);
+                    HttpRequestMessage requestMessage = new(HttpMethod.Put, $"https://api.cloudflare.com/client/v4/zones/{zoneIdentifier}/dns_records/{identifier}")
+                    {
+                        Content = new StringContent(bodyJson.ToJsonString(), Encoding.UTF8, MediaTypeNames.Application.Json)
+                    };
+                    HttpResponseMessage responseMessage = client.Send(requestMessage);
+
+                    logger.LogInformation("Update result: {statusCode} - {reasonPhrase}", responseMessage.StatusCode, responseMessage.ReasonPhrase);
+
+                    if (!responseMessage.IsSuccessStatusCode)
+                        logger.LogError("Error Response Content: {content}", responseMessage.Content.ReadAsStringAsync().Result);
+                }
+            }
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            logger.LogInformation("Stopping Cloudflare DNS Update service.");
+            _timer?.Change(Timeout.Infinite, 0);
+            return Task.CompletedTask;
+        }
+
+        public void Dispose()
+        {
+            _timer?.Dispose();
+        }
+    }
+}
--- CloudflareDNSUpdater/CloudflareDNSUpdater/Program.cs
+++ CloudflareDNSUpdater/CloudflareDNSUpdater/Program.cs
@@ -1,92 +1,41 @@
-﻿using CloudflareDNSUpdater;
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Console;
 using System.Net.Mime;
 using System.Reflection;
 using System.Text;
 using System.Text.Json.Nodes;
 
-
-string basePath = args[0];
-string configFile = Path.Combine(basePath, "cloudflare.config");
-string logFilePath = Path.Combine(basePath, "cloudflare.log");
-
-// if log file is from previous day, delete it
-FileInfo logFileInfo = new(logFilePath);
-if (logFileInfo.CreationTime < DateTime.Today)
-    logFileInfo.Delete();
-
-Logger.LogInfo(logFilePath, "Starting updating DNS records with the current ip");
-
-if (!File.Exists(configFile))
+namespace DSaladin.CloudflareDnsUpdater
 {
-    Logger.LogError(logFilePath, "The config file is missing");
-    throw new FileNotFoundException("The config file is missing", configFile);
-}
-
-string json = File.ReadAllText(configFile, Encoding.UTF8);
-Configuration? configuration = System.Text.Json.JsonSerializer.Deserialize<Configuration>(json);
-
-if (configuration is null)
-{
-    Logger.LogError(logFilePath, "Could not serialize the configuration");
-    throw new NullReferenceException("Could not serialize the configuration");
-}
-
-if (string.IsNullOrEmpty(configuration.BearerToken))
-{
-    Logger.LogError(logFilePath, "The bearer token is empty");
-    throw new NullReferenceException("The bearer token is empty");
-}
-
-if (configuration.ZoneIdentifiers is null || configuration.ZoneIdentifiers.Count == 0)
-{
-    Logger.LogError(logFilePath, "No zones found");
-    throw new NullReferenceException("No zones found");
-}
-
-HttpClient client = new();
-client.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", configuration.BearerToken);
-
-// Get current public/external ip
-HttpResponseMessage ipResponseMessage = new HttpClient().Send(new(HttpMethod.Get, "http://icanhazip.com"));
-string ip = ipResponseMessage.Content.ReadAsStringAsync().Result.Trim();
-Logger.LogInfo(logFilePath, $"Current ip address: {ip}");
-
-foreach (string zoneIdentifier in configuration.ZoneIdentifiers)
-{
-    Logger.LogInfo(logFilePath, $"Fetching zone dns entries (Identifier: {zoneIdentifier})");
-
-    HttpResponseMessage dnsRecordsResponseMessage = client.Send(new(HttpMethod.Get, $"https://api.cloudflare.com/client/v4/zones/{zoneIdentifier}/dns_records?type=A"));
-    JsonObject? dnsRecords = (JsonObject)JsonNode.Parse(dnsRecordsResponseMessage.Content.ReadAsStringAsync().Result)!;
-
-    if (dnsRecords == null || dnsRecords.Count == 0 || dnsRecords.First().Value == null || dnsRecords.First().Value!.GetType() != typeof(JsonArray))
-        continue;
-
-    foreach (JsonObject? dnsRecord in dnsRecords.First().Value!.AsArray().Cast<JsonObject?>())
+    class Program
     {
-        if (dnsRecord is null)
-            continue;
-
-        string identifier = dnsRecord["id"]!.GetValue<string>();
-        string dnsName = dnsRecord["name"]!.GetValue<string>();
-
-        JsonObject bodyJson = new()
+        public static void Main(string[] args)
         {
-            { "type", dnsRecord["type"]!.GetValue<string>() },
-            { "name", dnsName },
-            { "content", ip },
-            { "ttl", dnsRecord["ttl"]!.GetValue<int>() },
-            { "proxied", dnsRecord["proxied"]!.GetValue<bool>() }
-        };
+            CreateHostBuilder(args).Build().Run();
+        }
 
-        Logger.LogInfo(logFilePath, $"Sending request for DNS {dnsName} and identifier {identifier}");
-        HttpRequestMessage requestMessage = new(HttpMethod.Put, $"https://api.cloudflare.com/client/v4/zones/{zoneIdentifier}/dns_records/{identifier}")
-        {
-            Content = new StringContent(bodyJson.ToJsonString(), Encoding.UTF8, MediaTypeNames.Application.Json)
-        };
-        HttpResponseMessage responseMessage = client.Send(requestMessage);
-
-        Logger.LogInfo(logFilePath, $"DNS identifier change: {responseMessage.StatusCode}");
-        if (!responseMessage.IsSuccessStatusCode)
-            Logger.LogError(logFilePath, responseMessage.Content.ReadAsStringAsync().Result);
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureServices((hostContext, services) =>
+                {
+                    services.AddHostedService<LoggerHostedService>();
+                })
+                .ConfigureLogging(logging =>
+                {
+                    logging.ClearProviders();
+                    logging.AddConsole(options =>
+                    {
+                        options.FormatterName = ConsoleFormatterNames.Simple;
+                    });
+                    logging.AddSimpleConsole(options =>
+                    {
+                        options.IncludeScopes = false;
+                        options.SingleLine = true;
+                        options.TimestampFormat = "yyyy-MM-dd HH:mm:ss ";
+                        options.UseUtcTimestamp = false;
+                    });
+                });
     }
 }
